538e01713c8555d418eeb4d9a6a036a4f68c67b8
Bryan Sandoval <cebrerosbryan@gmail.com>
2022-05-18 15:30:48 -0600

    shows users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import NotFound from './pages/NotFound'
+import About from './pages/About'
+import Home from './pages/Home'
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 
@@ -9,7 +12,13 @@ function App() {
     {/* H-SCREEN + JUST-BET WORKS VERTICALLY */}
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/About' element={<About />}></Route>
+            <Route path='/*' element={<NotFound />}></Route>
+          </Routes>
+        </main>
         <Footer />
       </div>
       
